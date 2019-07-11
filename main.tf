0491108a9a2a3249dc33d8ea7abf5946c91327c5
naz121 <42747964+naz121@users.noreply.github.com>
2019-07-11 13:28:18 +0530

    changed test5 to test

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-resource "azurerm_resource_group" "test5" {
-  name     = "azure-functions-test55-rg"
+resource "azurerm_resource_group" "test" {
+  name     = "azure-functions-test-rg"
   location = "westus2"
 }
 
-resource "azurerm_storage_account" "test5" {
-  name                     = "functionsapptest5sa"
-  resource_group_name      = "${azurerm_resource_group.test5.name}"
-  location                 = "${azurerm_resource_group.test5.location}"
+resource "azurerm_storage_account" "test" {
+  name                     = "functionsapptestsa"
+  resource_group_name      = "${azurerm_resource_group.test.name}"
+  location                 = "${azurerm_resource_group.test.location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_app_service_plan" "test5" {
-  name                = "azure-functions-test5-service-plan"
-  location            = "${azurerm_resource_group.test5.location}"
-  resource_group_name = "${azurerm_resource_group.test5.name}"
+resource "azurerm_app_service_plan" "test" {
+  name                = "azure-functions-test-service-plan"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
 
   sku {
     tier = "Standard"
@@ -22,10 +22,10 @@ resource "azurerm_app_service_plan" "test5" {
   }
 }
 
-resource "azurerm_function_app" "test5" {
-  name                      = "test5-azure-functions"
-  location                  = "${azurerm_resource_group.test5.location}"
-  resource_group_name       = "${azurerm_resource_group.test5.name}"
-  app_service_plan_id       = "${azurerm_app_service_plan.test5.id}"
-  storage_connection_string = "${azurerm_storage_account.test5.primary_connection_string}"
+resource "azurerm_function_app" "test" {
+  name                      = "test-azure-functions"
+  location                  = "${azurerm_resource_group.test.location}"
+  resource_group_name       = "${azurerm_resource_group.test.name}"
+  app_service_plan_id       = "${azurerm_app_service_plan.test.id}"
+  storage_connection_string = "${azurerm_storage_account.test.primary_connection_string}"
 }
